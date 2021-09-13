e8e8eb93c57555ac2313fe83f1c36e5f13b17274
Halfiies <morriswillis@outlook.com>
2021-09-13 16:38:35 +0100

    basic css, some stolen ideas for a framework on js

---
+++ calulator.js
@@ -0,0 +1,53 @@
+// accept an input
+//text field + input buttons
+// if answer box is empty, copy input to answer on operator click 
+// const answerBox = " ";
+//answerBox = document.getElementsByClassName("answer").value;
+
+//function boxUpdate() {
+//  console.log(answerBox)};
+
+//if answer box is full, perform operation
+//const equals = document.getElementsByClassName("equals");
+
+//equals.addEventListener("click", test())
+ function test(){console.log("its working")};
+
+//check for numbers or symbols
+// on click read inner html
+// on number click, place number into input box
+//document.querySelectorAll("number").addEventlistener("click", updateNumber);
+//function updateNumber() => {}
+
+// 
+
+//perform basic maths
+//return answer
+
+const numberButtons = document.querySelectorAll(".number")
+const operationButtons = document.querySelectorAll(".operator")
+const equalsButton = document.querySelector(".equals")
+const deleteButton = document.querySelector(".delete")
+const clearButton = document.querySelector(".clear")
+const output= document.querySelector(".output")
+const input= document.querySelector(".input")
+const storedInput = document.querySelector(".storedInput")
+
+clear() {
+}
+
+delete() {
+}
+
+appendNumber(number) {
+}
+
+chooseOperation(operation) {
+}
+
+compute() {
+}
+
+updateOutput() {
+}
+
