e982be5a16b4d379da0129ea373ba3b6bf24260c
Halfiies <morriswillis@outlook.com>
2021-09-14 15:18:18 +0100

    inout working

--- calulator.js
+++ calulator.js
@@ -11,7 +11,7 @@
 //const equals = document.getElementsByClassName("equals");
 
 //equals.addEventListener("click", test())
- function test(){console.log("its working")};
+console.log("its working")
 
 //check for numbers or symbols
 // on click read inner html
@@ -30,24 +30,18 @@ const equalsButton = document.querySelector(".equals")
 const deleteButton = document.querySelector(".delete")
 const clearButton = document.querySelector(".clear")
 const output= document.querySelector(".output")
-const input= document.querySelector(".input")
+let input= ""
 const storedInput = document.querySelector(".storedInput")
-
-clear() {
-}
-
-delete() {
-}
-
-appendNumber(number) {
-}
-
-chooseOperation(operation) {
-}
-
-compute() {
-}
-
-updateOutput() {
-}
-
+for (let i=0; i< numberButtons.length;i++){
+  numberButtons[i].addEventListener("click", function () {
+    input += numberButtons[i].innerText;
+    console.log(input);
+    document.querySelector(".input").value = input;
+  });
+};
+for (let i=0; i<operationButtons.length; i++){
+  operationButtons[i].addEventListener("click", function () {
+    console.log("this is an operator button");
+  });
+};
+document.querySelector(".input").innerText = input
