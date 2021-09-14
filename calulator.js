4aa1843b8683cc231d7a33e303033d762325daba
Halfiies <morriswillis@outlook.com>
2021-09-14 15:51:17 +0100

    working

--- calulator.js
+++ calulator.js
@@ -11,7 +11,7 @@
 //const equals = document.getElementsByClassName("equals");
 
 //equals.addEventListener("click", test())
-console.log("its working")
+
 
 //check for numbers or symbols
 // on click read inner html
@@ -29,19 +29,27 @@ const operationButtons = document.querySelectorAll(".operator")
 const equalsButton = document.querySelector(".equals")
 const deleteButton = document.querySelector(".delete")
 const clearButton = document.querySelector(".clear")
-const output= document.querySelector(".output")
+let answer= ""
 let input= ""
-const storedInput = document.querySelector(".storedInput")
 for (let i=0; i< numberButtons.length;i++){
   numberButtons[i].addEventListener("click", function () {
     input += numberButtons[i].innerText;
-    console.log(input);
     document.querySelector(".input").value = input;
   });
 };
 for (let i=0; i<operationButtons.length; i++){
   operationButtons[i].addEventListener("click", function () {
-    console.log("this is an operator button");
+    input += operationButtons[i].innerText;
+    document.querySelector(".input").value = input;
   });
 };
-document.querySelector(".input").innerText = input
+deleteButton.addEventListener( "click", function () {
+ input= input.slice(0,-1);
+ document.querySelector(".input").value = input;
+});
+
+
+equalsButton.addEventListener("click", function(){
+  document.querySelector(".answer").value = eval(input);
+  document.querySelector(".input").value = "";
+});
